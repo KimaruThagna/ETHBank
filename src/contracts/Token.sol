217f128c60f0d16be069473a5097e1962401a153
Kimaru thagana <macbuntu@192.168.1.2>
2021-05-13 14:27:04 +0300

    token contract

---
+++ src/contracts/Token.sol
@@ -0,0 +1,28 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.6.0 <0.8.0;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+//inheritance from ERC20 contract
+contract Token is ERC20 {
+  //add minter variable
+  address public minter;
+  //add minter changed event
+  event MinterChanged(address indexed from, address to);
+
+  constructor() public payable ERC20("KimaruCoins", "KTC") {
+    // account that mints the tokens is the account that deployed the contract
+    minter = msg.sender;
+  }
+
+  // pass minter role function
+  function passMinterRole(address dBank) public returns(bool) {
+    minter = dBank;
+    emit MinterChanged(msg.sender, dBank);
+    return true;
+  }
+  
+  function mint(address account, uint256 amount) public {
+    //check if msg.sender have minter role
+		_mint(account, amount);
+	}
+}
